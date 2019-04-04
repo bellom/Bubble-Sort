de618c138b5d972d310c8e20dcb2a1d994208b60
bellom <bellomsean@gmail.com>
2019-04-04 15:57:28 +0100

    PR correction

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,16 +1,5 @@
 def bubble_sort_by (arr)
-    swapped = false
-    until swapped
-      swapped = true
-      0.upto(arr.length-2) do |i|
-        value = yield arr[i], arr[i+1]
-        if value > 0
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-          swapped = false
-        end
-      end
-    end
-    arr
+
 end
   
 bubble_sort_by(["hi","hello","hey"]) {|left,right| left.length - right.length}
