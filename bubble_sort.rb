83df215ba1f8ee6d5feca21f4c76e1316c258309
bellom <bellomsean@gmail.com>
2019-04-04 19:28:53 +0100

    merge erro fixed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,15 @@
 def bubble_sort (arr)
-
+    swapped = false
+    until swapped
+      swapped = true
+      0.upto(arr.length-2) do |i|
+        if arr[i] > arr[i+1]
+          arr[i], arr[i+1] = arr[i+1], arr[i]
+          swapped = false
+        end
+      end
+    end
+    return arr
 end
   
 bubble_sort([4,3,78,2,0,2])
